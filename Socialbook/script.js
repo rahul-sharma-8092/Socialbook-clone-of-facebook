42cc40b85f7d4d8f7cd7c58cce4d9888836a8133
Rahul Kumar Sharma <124697050+rahul-sharma-8092@users.noreply.github.com>
2023-05-22 23:05:20 +0530

    Dark mode and Profile page added

--- Socialbook/script.js
+++ Socialbook/script.js
@@ -5,3 +5,26 @@ const settingMenu = document.querySelector(".setting-menu");
 profilePic.addEventListener("click", () => {
 	settingMenu.classList.toggle("settings-menu-height");
 });
+
+// Dark button
+const darkBtn = document.getElementById("dark-btn");
+darkBtn.addEventListener("click", () => {
+	darkBtn.classList.toggle("dark-btn-on");
+	document.body.classList.toggle("dark-theme");
+
+	if (localStorage.getItem("theme") == "light") {
+		localStorage.setItem("theme", "dark");
+	} else {
+		localStorage.setItem("theme", "light");
+	}
+});
+
+if (localStorage.getItem("theme") == "light") {
+	darkBtn.classList.remove("dark-btn-on");
+	document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+	darkBtn.classList.add("dark-btn-on");
+	document.body.classList.add("dark-theme");
+} else {
+	localStorage.setItem("theme", "light");
+}
